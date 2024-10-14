27f64db8ddd5b87cb2bce109de9b8b58fa80fb09
LaHuella <german.dosaula@gmail.com>
2024-10-14 18:30:02 +0000

    trabajo Terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    const quien = who[Math.floor(Math.random() * who.length)];
+    const que = action[Math.floor(Math.random() * action.length)];
+    const cosa = what[Math.floor(Math.random() * what.length)];
+    const cuando = when[Math.floor(Math.random() * when.length)];
+
+    const excusa = `${quien} ${que} ${cosa} ${cuando}.`;
+
+    document.getElementById("excusa").innerHTML = excusa;
+  }
+
+  generateExcuse();
+  document
+    .getElementById("botonExcusa")
+    .addEventListener("click", generateExcuse);
 };
